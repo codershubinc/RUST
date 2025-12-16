78f5f5e2a7e8f0307fdf9b8e3a5479ac12bc0972
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-12-16 23:02:08 +0530

    feat: enhance command input handling with shorthand options

--- user_store/src/main.rs
+++ user_store/src/main.rs
@@ -9,29 +9,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         println!("Please enter a command (add, list, remove exit):");
         let mut command: String = String::new();
-        std::io::stdin().read_line(&mut command);
+        let _ = std::io::stdin().read_line(&mut command);
         let command = command.trim();
         match command {
-            "add" => {
+            "add" | "a" => {
                 println!("Enter user name:");
                 let mut name = String::new();
-                std::io::stdin().read_line(&mut name);
+                let _ = std::io::stdin().read_line(&mut name);
                 let name = name.trim();
-                db::add_user(&sql, name).await;
+                let _ = db::add_user(&sql, name).await;
                 println!("User '{}' added.", name);
             }
-            "exit" => {
+            "exit" | "e" => {
                 println!("Exiting...");
                 break;
             }
-            "list" => {
+            "list" | "l" => {
                 println!("Listing users:");
                 db::show_all_users(&sql).await?;
             }
-            "remove" => {
+            "remove" | "r" => {
                 println!("Enter user ID to remove:");
                 let mut id_str = String::new();
-                std::io::stdin().read_line(&mut id_str);
+                let _ = std::io::stdin().read_line(&mut id_str);
                 let id: i64 = match id_str.trim().parse() {
                     Ok(num) => num,
                     Err(_) => {
